23cb24d2448665d579a45a335999520c57327c3c
Sourav <sourav.iit.nstu@gmail.com>
2022-10-19 20:31:49 +0600

    add all

---
+++ src/data_structure/linkedlist/Main.java
@@ -0,0 +1,17 @@
+package data_structure.linkedlist;
+
+class Node{
+    int data;
+    Node next;
+}
+
+class Linkedlist{
+    Node head;
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
