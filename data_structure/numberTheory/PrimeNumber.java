2c062871e2d4e2b9a67d39ebbea5806cc8b2c6a4
Sourav <sourav.iit.nstu@gmail.com>
2022-10-28 13:44:22 +0600

    prime check

--- data_structure/numberTheory/PrimeNumber.java
+++ data_structure/numberTheory/PrimeNumber.java
@@ -1,4 +1,25 @@
 package data_structure.numberTheory;
 
+import java.util.Scanner;
+
 public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        boolean isPrime = checkPrime(number);
+         if (isPrime == true){
+             System.out.println("Prime");
+         }else System.out.println("Not Prime");
+    }
+
+    private static boolean checkPrime(int number) {
+         boolean n = true;
+        for (int i = 2; i < number;i++){
+            if (number%i == 0){
+                n = false;
+
+            }
+        }
+       return n;
+    }
 }
