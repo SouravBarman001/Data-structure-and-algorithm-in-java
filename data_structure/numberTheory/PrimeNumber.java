4caee2ed2867825b01621211c3f5b47f703aa26b
Sourav <sourav.iit.nstu@gmail.com>
2022-10-28 14:21:09 +0600

    add test case

--- data_structure/numberTheory/PrimeNumber.java
+++ data_structure/numberTheory/PrimeNumber.java
@@ -5,18 +5,32 @@ import java.util.Scanner;
 public class PrimeNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int number = sc.nextInt();
-        boolean isPrime = checkPrime(number);
-         if (isPrime == true){
-             System.out.println("Prime");
-         }else System.out.println("Not Prime");
+
+        System.out.println("Enter test case:");
+        int Tcase = sc.nextInt();
+       for (int i = 0;i < Tcase;i++ )
+        {
+
+            int number = sc.nextInt();
+            boolean isPrime = checkPrime(number);
+            if (isPrime == true) {
+                System.out.println("Prime");
+            } else
+            {
+                System.out.println("Not Prime");
+            }
+
+        }
     }
 
     private static boolean checkPrime(int number) {
          boolean n = true;
+         if (number < 2){n=true;}
+         if (number <= 3){n=true;}
+         if (number%2 == 0){n = false;}
          int num = (int) Math.sqrt(number);
      //   System.out.println(num);
-        for (int i = 2; i <= num;i++){
+        for (int i = 3; i <= num;i++){
             if (number%i == 0){
                 n = false;
 
