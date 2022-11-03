74964467b38bd2025b513f2a987a537bf53ff5cb
Sourav <sourav.iit.nstu@gmail.com>
2022-11-03 08:18:09 +0600

    Sieve of Eratosthenes

--- data_structure/numberTheory/SieveOfEratosthenes.java
+++ data_structure/numberTheory/SieveOfEratosthenes.java
@@ -9,37 +9,38 @@ public class SieveOfEratosthenes {
         int arrSize = sc.nextInt(); // 30
 
         sieveFunction(arrSize);
-
-
-
-
-
-
-
     }
 
     private static void sieveFunction(int n) {
 
-        int sieveArray[] = new int[30];
+        boolean prime[] = new boolean[n+1];
 
-        for (int i=0;i<sieveArray.length;i++){
-            sieveArray[i]=0;
+        for (int i=2;i<=n;i++){
+            prime[i]=true;  // initially marked every number as prime
         }
 
-        for (int i=2;i<n;i++){
-            if (sieveArray[i] == 0){
-                for (int j=i*i;j<n;j=j+i){
-                    sieveArray[i] = 1;
-                }
+       // int num = (int) Math.sqrt(n);
+
+        for (int i = 2; i <=Math.sqrt(n) ; i++){
+          if (prime[i]){
+              for (int j=i;j*i<=n;j++){
+                  prime[j*i] = false;
+              }
+          }
+        }
+        int count = 0;
+        for (int i = 2; i <= n; i++){
+            if (prime[i]){
+                count++;
+                System.out.print(i+" ");
             }
         }
+        System.out.println("");
+        System.out.println("Count = "+count);
 
-        for (int i=2;i<n;i++){
-            if (sieveArray[i]==0){
-                System.out.print(i);
-                System.out.println(",");
-            }
-        }
+
+
+
 
     }
 }
